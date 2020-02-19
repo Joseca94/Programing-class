3c64a6e39ac53299b6315a6991804c5a6b70b3ce
Joseca94 <61141470+Joseca94@users.noreply.github.com>
2020-02-19 11:49:54 +0100

    Add files via upload

---
+++ clase 3.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace clase_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Escribe un numero: ");
+            //int numero = int.Parse(Console.ReadLine());
+            //Console.WriteLine("escribe otro numero: ");
+            //int numero1 = int.Parse(Console.ReadLine());
+
+            //if(numero == numero1)
+            //{
+
+            //}
+            //else
+            //{
+            //    if(numero > numero1)
+            //    {
+
+            //    }
+            //    else
+            //    {
+
+            //    }
+            //}
+
+            //if (numero == numero1)
+            //{
+            //    Console.WriteLine("son iguales");
+            //}
+            //else if (numero > numero1)
+            //{
+            //    Console.WriteLine("el primero es mayor");
+            //}
+            //else if (numero1 > numero)
+            //{
+            //    Console.WriteLine("el segundo es mayor");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no son iguales");
+            //Console.WriteLine("escribe un numero: ");
+            //int y = int.Parse(Console.ReadLine());
+            //if (y % 2 == 0)
+            //{
+            //    Console.WriteLine("es par");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("es impar");
+            //}
+            //Console.WriteLine("escribe un numero: ");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine("escribre otro numero");
+            //int x = int.Parse(Console.ReadLine());
+            //if (y % x == 0)
+            //{
+            //    Console.WriteLine("es multiplo");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no es multiplo");
+            //}
+            //Console.WriteLine("escribe un numero: ");
+            //int y = int.Parse(Console.ReadLine());
+            //if (y == 0)
+            //{
+            //    Console.WriteLine("es 0 ");
+
+            //}
+            //else
+            //{
+            //    if (y > 0)
+            //    {
+            //        Console.WriteLine("es positivo");
+            //    }
+            //    else
+            //        Console.WriteLine("es negativo");
+            //}
+            //Console.WriteLine("escribe un numero: ");
+            //int y = int.Parse(Console.ReadLine());
+
+            //switch(y)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Cuando el grajo vuela bajo hace un frio del carajo");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("van dos y se cae el del medio");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Mongolo");
+            //        break;
+            //    default:
+            //        Console.WriteLine("error");
+            //        break;
+
+
+
+            //}
+            //Console.WriteLine("escribe tu nota 1 : ");
+            //float x = float.Parse(Console.ReadLine());
+            //Console.WriteLine("escribe tu nota 2 : ");
+            //float z = float.Parse(Console.ReadLine());
+            //Console.WriteLine("escribe tu nota 3 : ");
+            //float y = float.Parse(Console.ReadLine());
+            //Console.Write("tu media es: " + (x + z + y) / 3f);
+
+            //Console.WriteLine("escribe tu altura sin comas: ");
+            //float x = float.Parse(Console.ReadLine());
+            //if (x >= 140 && x <= 160)
+            //{
+            //    Console.WriteLine("eres un enano");
+            //}
+            //else if (x >= 160 && x <= 180)
+            //{
+            //    Console.WriteLine("eres normal");
+            //}
+            //else if(x >= 180 && x <= 200)
+            //{
+            //    Console.WriteLine("eres alto");
+            //}
+
+
+
+
+
+        }
+}   }
+
